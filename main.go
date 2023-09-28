b621adab8705967f59c53e850453b050b959825e
pegov <dvpegov@gmail.com>
2023-09-28 13:10:28 +0300

    implement random ai

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"os/exec"
 	"runtime"
 	"strconv"
+	"time"
 )
 
 func runCmd(name string, arg ...string) {
@@ -45,7 +48,23 @@ func drawBoard(b [9]int) {
 	}
 }
 
+func AIRandomNextMove(b [9]int) int {
+	choices := make([]int, 0, 9)
+	for i, v := range b {
+		if v == 0 {
+			choices = append(choices, i)
+		}
+	}
+
+	return choices[rand.Intn(len(choices))]
+}
+
 func main() {
+	ai := flag.Bool("ai", false, "to play vs ai")
+	flag.Parse()
+
+	rand.Seed(time.Now().Unix())
+
 	gameOver := false
 	winner := 0
 
@@ -79,32 +98,37 @@ func main() {
 			fmt.Println(status)
 		}
 
-		var moveIndex string
-		fmt.Scan(&moveIndex)
+		if player == 2 || !*ai {
+			var moveIndex string
+			fmt.Scan(&moveIndex)
 
-		if moveIndex == "q" {
-			fmt.Println("Exiting")
-			os.Exit(0)
-		}
-
-		switch moveIndex {
-		case "1", "2", "3", "4", "5", "6", "7", "8", "9":
-			idx, _ := strconv.Atoi(string(moveIndex))
-			fmt.Println(idx)
-			idx--
-			if board[idx] != 0 {
-				status = fmt.Sprintf("%v is already occupied", idx+1)
-				continue
-			} else {
-				board[idx] = player
-				turn++
+			if moveIndex == "q" {
+				fmt.Println("Exiting")
+				os.Exit(0)
 			}
-			status = ""
-			break
-		default:
-			status = "1-9 or q to quit"
-			continue
+
+			switch moveIndex {
+			case "1", "2", "3", "4", "5", "6", "7", "8", "9":
+				idx, _ := strconv.Atoi(string(moveIndex))
+				fmt.Println(idx)
+				idx--
+				if board[idx] != 0 {
+					status = fmt.Sprintf("%v is already occupied", idx+1)
+					continue
+				} else {
+					board[idx] = player
+				}
+				status = ""
+				break
+			default:
+				status = "1-9 or q to quit"
+				continue
+			}
+		} else {
+			board[AIRandomNextMove(board)] = player
 		}
+
+		turn++
 
 		winCombinations := [8][3]int{
 			{0, 1, 2},
